26b20e8ac438d84dbc27839c86228d6a29830257
peter <fr33wayz@gmail.com>
2018-09-07 14:06:58 +0100

    update configs

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -116,3 +116,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/assets/'
+LOGGING = {
+      'version': 1,
+      'disable_existing_loggers': False,
+      'handlers': {
+          'console': {
+              'class': 'logging.StreamHandler',
+          },
+      },
+      'loggers': {
+          'django': {
+              'handlers': ['console'],
+              'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+          },
+      },
+  }
--- project_name/settings/prod.py
+++ project_name/settings/prod.py
@@ -1,15 +1,15 @@
 from base import *
+from {{ project_name | lower }}.utils import is_heroku
 import dj_database_url
 
 
 PRODUCTION_APP = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-
 ]
 
 PRODUCTION_MIDDLEWARE = [
-        'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 MIDDLEWARE_CLASSES = DEV_MIDDLEWARE_CLASSES + PRODUCTION_MIDDLEWARE + PROJECT_MIDDLEWARE_CLASSES
@@ -18,10 +18,11 @@ DEBUG = False
 
 
 # use the heroku postgres database
-django_database_config = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(django_database_config)
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+if is_heroku():
+    django_database_config = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(django_database_config)
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['*']
 
@@ -35,5 +36,4 @@ STATICFILES_DIRS = [
     )
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
